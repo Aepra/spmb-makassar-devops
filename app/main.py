b5c6e69f0476875f9eff96933ac4e6c2bac25281
Abel Eka Putra <abelekaputra05@gmail.com>
2026-02-28 14:36:07 +0800

    feat: implement cloudflare tunnel and fix 502 gateway for public api

--- app/main.py
+++ app/main.py
@@ -29,3 +29,8 @@ def read_data(
         "limit": limit,
         "data": rows
     }
+
+# Tambahkan ini di baris paling bawah main.py
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
