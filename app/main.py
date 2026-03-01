7a08c5e76568d02f3a82509bca9e0ca3f9dcfd2b
Abel Eka Putra <abelekaputra05@gmail.com>
2026-03-01 20:46:27 +0800

    tes perubahan

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def startup_event():
 # --- ENDPOINT API ---
 @app.get("/")
 def root():
-    return {"message": "API PPDB Makassar Aktif!", "owner": "Abel Eka Putra", "github": "https://github.com/Aepra"}
+    return {"message": "API PPDB Makassar Aktif!", "owner": "Abel Eka Putra", "github": "https://github.com/Aepra", "tes": "berhasil mengubah"}
 
 @app.get("/data")
 def read_data(
